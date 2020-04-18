455b012dd03983dfb0bf53ab97a44880c5d7883f
Vins <vins@localhost.localdomain>
2020-04-18 14:13:27 +0900

    Edit

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 import styled from 'styled-components';
 
 const Header = styled.header`
@@ -22,26 +22,27 @@ const List = styled.ul`
 `;
 
 const Item = styled.li`
-  width: 50px;
-  height: 100%;
+  width: 80px;
+  height: 50px;
   text-align: center;
-  &:not(:last-child) {
-  margin-right: 10px;
-  }
+  border-bottom: 3px solid ${props => (props.current ? "#3498db" : "transparent")};
+  transition: border-bottom 0.36s ease-out;
 `;
 
 const SLink = styled(Link)`
-  height: 100%;
+  height: 50px;
   display: flex;
   align-items: center;
   justify-content: center;
 `;
-export default () => (
-    <Header>
-        <List>
-            <Item><SLink to="/">Movies</SLink></Item>
-            <Item><SLink to="/tv">TV</SLink></Item>
-            <Item><SLink to="/search">Search</SLink></Item>
-        </List>
-    </Header>
+export default withRouter(
+    ({location: {pathname}}) => (
+        <Header>
+            <List>
+                <Item current={pathname === "/"}><SLink to="/">Movies</SLink></Item>
+                <Item current={pathname === "/tv"}><SLink to="/tv">TV</SLink></Item>
+                <Item current={pathname === "/search"}><SLink to="/search">Search</SLink></Item>
+            </List>
+        </Header>
+    )
 )
