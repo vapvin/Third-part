20d77a6d358f6f7348364076e4972e0bd79e51c7
Vins <vins@localhost.localdomain>
2020-04-21 00:43:48 +0900

    Updates

---
+++ precache-manifest.8ba9605e0d95cdf43beff3f0589fdc85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34ffd042efdaedef8c7b28e1e10ad0cb",
+    "url": "/Third-part/index.html"
+  },
+  {
+    "revision": "1fe1c73fb0275b048bd4",
+    "url": "/Third-part/static/js/2.e3c4c480.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/Third-part/static/js/2.e3c4c480.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "424bf3b247b4a2002a4c",
+    "url": "/Third-part/static/js/main.8fbfb41d.chunk.js"
+  },
+  {
+    "revision": "25f84f837a6151908072",
+    "url": "/Third-part/static/js/runtime-main.547c9aba.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/Third-part/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
